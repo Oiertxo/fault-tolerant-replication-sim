4cd26527d2ba1d855e62570ad9aec10df0498d43
Oier Layana <alumno@eim-alu-69184.lab.unavarra.es>
2024-11-13 16:00:13 +0100

    Objeto de replica realizado

--- replica.js
+++ replica.js
@@ -1,6 +1,6 @@
 /**
  * EJECUCION
- * node objeto.js nombre_objeto nombre_bbdd
+ * node replica.js nombre_objeto nombre_bbdd
  */
 "use strict"
 
@@ -14,48 +14,90 @@ const sock = zmq.socket('dealer');
 sock.identity = process.argv[2];
 
 // El segundo argumento es el nombre de la base de datos del objeto
-const db = new Level(`./BBDD/${process.argv[3]}`);
+const db = new Level(`./bbdd/${process.argv[3]}`);
 
+// Conectar con el proxy
 sock.connect("tcp://127.0.0.1:2221");
+
+// Variables para objetos replicados
+let toexecute = []; // a vector of pairs (rhid, cmd). The first index is 1. Initially, ∀i : toexecute[i] = null;
+let executed = [];  // a vector of pairs (cmd, res). The first index is 1. Initially, ∀i : executed[i] = null;
+let expectseq = 1;  // a positive integer. It indicates the first null position in toexecute. Initially, expectseq = 1
 
 // Función para manejar mensajes recibidos
 sock.on('message', function (...args) {
     const message = JSON.parse(args[1].toString());
 
+    // Comprobar formato del mensaje
     if (!message) {
         console.error("Invalid message format.");
         return;
     }
 
-    console.log(message.dest,
-        `\x1b[32m: Solicitud recibida de\x1b[0m ${message.source}\x1b[32m. Ejecutando en base de datos\x1b[0m`);
-
-    // Preparar el mensaje de respuesta
-    const resp_message = {
-        ...message,
-        tag: "REPLY",
-        source: sock.identity,
-        dest: message.source
-    };
-
-
-    // Manejar operación 'get'
-    if (message.cmd.op.name === 'get') {
-        db.get(message.cmd.op.args, (err, value) => {
-            resp_message.res = err ? `error: ${err.message}` : value;
-            sock.send(['', JSON.stringify(resp_message)]);
-        });
+    // Descartar mensajes no dirigidos a este objeto o con secuencia incorrecta
+    if (message.dest !== process.argv[2] || message.tag !== "TOREQUEST" || message.seq <= 0) {
+        console.error("The message is discarded.");
+        return;
     }
-    // Manejar operación 'put'
-    else if (message.cmd.op.name === 'put') {
-        const [key, value] = message.cmd.op.args.split(" ");
-        db.put(key, value, (err) => {
-            resp_message.res = err ? `error: ${err.message}` : 'OK';
-            sock.send(['', JSON.stringify(resp_message)]);
-        });
-    } else {
-        resp_message.res = `error: unsupported operation ${message.cmd.op.name}`;
-        sock.send(['', JSON.stringify(resp_message)]);
+
+    // Comprobar si el mensaje es el esperado
+    if (message.seq === expectseq) {
+        toexecute[expectseq] = { source: message.source, cmd: message.cmd };
+
+        while (toexecute[expectseq] !== null) {
+            let { source, cmd } = toexecute[expectseq];
+
+            const resp_message = {
+                ...message,
+                tag: "TOREPLY",
+                cmd: cmd,
+                source: process.argv[2],
+                dest: source
+            };
+
+            // Manejar operación 'get'
+            if (cmd.op.name === 'get') {
+                db.get(cmd.op.args, (err, value) => {
+                    resp_message.res = err ? `error: ${err.message}` : value;
+                    sock.send(['', JSON.stringify(resp_message)]);
+                });
+            }
+            // Manejar operación 'put'
+            else if (cmd.op.name === 'put') {
+                const [key, value] = cmd.op.args.split(" ");
+                db.put(key, value, (err) => {
+                    resp_message.res = err ? `error: ${err.message}` : 'OK';
+                    sock.send(['', JSON.stringify(resp_message)]);
+                });
+            } else {
+                resp_message.res = `error: unsupported operation ${cmd.op.name}`;
+                sock.send(['', JSON.stringify(resp_message)]);
+            }
+
+            // Añadir comando a executed
+            executed[expectseq] = { cmd: cmd, res: resp_message.res };
+
+            // Actualizar expectseq
+            expectseq++;
+        }
+    } else if (message.seq < expectseq) { // Si el mensaje ya fue ejecutado
+        // Almacenar el mensaje para ejecutarlo más tarde
+        toexecute[message.seq] = { source: message.source, cmd: message.cmd };
+    } else { // Si el mensaje es para ejecutar en el futuro
+        let { cmd, res } = executed[message.seq];
+
+        // Preparar el mensaje de respuesta
+        let msgres = {
+            'source': process.argv[2],
+            'dest': message.source,
+            'tag': "TOREPLY",
+            'seq': message.seq,
+            'cmd': cmd,
+            'res': res
+        };
+
+        // Enviar la respuesta
+        sock.send(['', JSON.stringify(msgres)]);
     }
 });
 

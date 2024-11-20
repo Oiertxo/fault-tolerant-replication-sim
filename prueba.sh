ed455c38bcf3920db8db320f178ece730c98be11
Mihail Cojocaru <cojocaru.138604@e.unavarra.es>
2024-11-20 16:11:31 +0100

    Borrar pruebas.sh

--- prueba.sh
+++
@@ -1,5 +0,0 @@
-clientes=$(grep '"clientes"' config.json | awk -F': ' '{print $2}' | tr -d ', ')
-replicas=$(grep '"replicas"' config.json | awk -F': ' '{print $2}' | tr -d ', ')
-
-echo "Clientes: $clientes"
-echo "Replicas: $replicas"
